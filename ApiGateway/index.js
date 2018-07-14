1e0a1623ce3a9563f4fd7d70b9d0e4665cc1ffe1
Kasun Vithanage <alankasun@gmail.com>
2018-07-14 18:02:32 +0530

    api gateway

---
+++ ApiGateway/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const axios = require('axios')
+const app = express()
+
+const QUOTES_API_GATEWAY = process.env.QUOTES_API
+
+app.get('/api/status', (req, res) => {
+    return res.json({status: 'ok'})
+})
+
+app.get('/api/randomquote',async (req, res) => {
+    try {
+        const url = QUOTES_API_GATEWAY + '/api/qoute'
+        const quote = await axios.get(url)
+        return res.json({
+            time: Date.now(),
+            quote: quote.data
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500)
+        return res.json({
+            message: "Internal server error",
+        })
+    }
+    
+})
+
+app.listen(3000, () => {
+    console.log(process.env.QUOTES_API)
+    console.log('API Gateway is listening on port 3000!')
+})
